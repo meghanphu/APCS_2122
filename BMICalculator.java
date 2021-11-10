35587c3f9dff70a2e73156653c8abc3024b35628
meghanphu <89647949+meghanphu@users.noreply.github.com>
2021-11-09 20:50:21 -0500

    Add files via upload

--- BMICalculator.java
+++ BMICalculator.java
@@ -8,54 +8,57 @@ import java.util.Scanner;
  * @author MeghanPhu
  */
 public class BMICalculator {
-    /** Convert English to metric units, perform the BMI calculation.
-     * NOTE: this method must properly handle bad data */
+    /**
+     * Convert English to metric units, perform the BMI calculation.
+     * NOTE: this method must properly handle bad data
+     */
 
     public static double computeBMI(int inches, int pounds) {
         //1 inch == 0.0254 meters
         //1 pound == 0.454 kg
-        if(inches <= 0 || pounds < 0)
+        if (inches <= 0 || pounds < 0)
             return 0;
-        return pounds*.454 / Math.pow(inches*.0254, 2);
+        return pounds * .454 / Math.pow(inches * .0254, 2);
     }
 
     public static int extractInches(String value) {
-        //takes feet value until first ' and inches value from ' to "
         int qtPos = value.indexOf("'");
         int dblQtPos = value.indexOf("\"");
         if (qtPos == -1 || dblQtPos == -1) {
-            return -1;
-        }
+            //return -1; }
+            System.out.println("Invalid input "); }
         int feet = Integer.parseInt(value.substring(0, qtPos));
         int inches = Integer.parseInt(value.substring(qtPos + 1, dblQtPos));
 
         if (inches < 0 || inches > 11 || feet < 0)
-            return -1;
+            //return -1;
+            System.out.println("Invalid input ");
 
         return (feet * 12 + inches == 0) ? -1 : feet * 12 + inches;
     }
 
-    /** Uses a Scanner to prompt the user for info, process the
+    /**
+     * Uses a Scanner to prompt the user for info, process the
      * feet/inches conversion, calls the computeBMI method and prints the
-     * correct information. */
+     * correct information.
+     */
     public static void main(String[] args) {
-        while(true) {
-            Scanner in = new Scanner(System.in);
-            DecimalFormat df = new DecimalFormat("0.00");
+        Scanner in = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+        int inches, weight;
+        while (true) {
             try {
                 System.out.print("Enter your height in the format ft'in\":  ");
+                inches = extractInches(in.nextLine());
                 // takes in user's height
-                int inches = extractInches(in.nextLine());
-            } catch (Exception e) {
-                System.err.println("please input correct measurements for height"); }
-            try {
                 System.out.print("Enter your weight in pounds: ");
-                int weight = in.nextInt();
+                weight = in.nextInt();
                 // takes in user's weight
-            } catch (Exception w) {
-                System.err.println("please input correct measurements for weight"); }
-            System.out.println("Your BMI is: " + df.format(computeBMI(inches, weight)));
+                System.out.println("Your BMI, expressed as weight(kg)/height(m)^2: " + df.format(computeBMI(inches, weight)) + " kg/m^2");
                 // prints BMI
+            } catch (Exception e) {
+                System.out.println("Please enter the correct input for height (ft'in\") and weight ");
+                continue; }
             in.nextLine(); //Scanners are weird - this handles the nextInt buffer
             System.out.print("Continue (Y/N): ");
             String cont = in.nextLine();
@@ -65,4 +68,5 @@ public class BMICalculator {
         System.out.println("End of program");
     }
 }
-
+// couldn't figure out how to handle invalid data for weight
+// if the user inputs anything greater than 12 for inches, the program says Invalid input but doesn't do anything about it
